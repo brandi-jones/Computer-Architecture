a0019429af571cce83a93023a45688fb9eb814c9
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-07-22 14:33:57 -0400

    day1/2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,33 +2,58 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
+        self.running = True
 
-    def load(self):
+    def load(self, file_name):
         """Load a program into memory."""
 
-        address = 0
+        # address = 0
 
-        # For now, we've just hardcoded a program:
+        # # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        try:
+            address = 0
+            with open(file_name) as file:
+                for line in file:
+                    split_line = line.split('#')[0]
+                    command = split_line.strip()
+                    if command == '':
+                        continue
+                    instruction = int(command, 2)
+                    self.ram[address] = instruction
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} file not found")
+            sys.exit()
+
 
 
     def alu(self, op, reg_a, reg_b):
@@ -37,6 +62,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -60,6 +87,46 @@ class CPU:
 
         print()
 
+    def ram_read(self, MAR): #MAR - memory address register
+        return self.ram[MAR] 
+
+    def ram_write(self, MAR, MDR): #MDR- memory data register
+        self.ram[MAR] = MDR
+
+    #this instruction sets a specified register to a specified value
+    def ldi(self, regNum, value):
+        self.reg[regNum] = value
+
+    def prn(self, regNum):
+        print(self.reg[regNum])
+
+    def hlt(self):
+        self.running = False
+
     def run(self):
         """Run the CPU."""
-        pass
+        
+
+        while self.running:
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            instructionRegister = self.ram[self.pc]
+
+            if instructionRegister == LDI:
+                self.ldi(operand_a, operand_b)
+                self.pc += 3
+
+            elif instructionRegister == PRN:
+                self.prn(operand_a)
+                self.pc += 2
+
+            elif instructionRegister == HLT:
+                self.hlt()
+                self.pc += 1
+
+            elif instructionRegister == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
+
+            else:
+                self.pc += 1
